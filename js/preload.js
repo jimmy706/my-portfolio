f65066093930b735c4e201abe1102318afd90383
Jimmy <jim.dangquoc706@gmail.com>
2019-08-31 19:44:05 -0700

    add preloader

---
+++ js/preload.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        const preload = document.getElementById("preload");
+        preload.classList.add("loaded");
+    }, 500)
+})
