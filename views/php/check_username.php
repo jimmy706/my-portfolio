e341a3622601972d5ed3504ec143dd14c1c5fa21
Jimmy <jim.dangquoc706@gmail.com>
2019-12-02 20:27:26 +0700

    update portfolio

--- views/php/check_username.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    require "connect.php";
-    $username = $_GET['username'];
-    $sql = "SELECT * from thanhvien where tendangnhap = '$username' ";
-    $result = $connect->query($sql);
-    echo mysqli_num_rows($result);
-    $connect->close();
-?>
--- views/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['userId']);
-    header("Location: ./index.html");
-    die();
-?>
